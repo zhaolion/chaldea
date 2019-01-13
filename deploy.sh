6217c5a440687bf98f3c7fae7f01b532ccba63b2
zhaolion <zhaoliangsyn@gmail.com>
2019-01-13 18:42:25 +0800

    deploy and add CNAME

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,9 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
+# add CNAME
+cp dist/CNAME dist/public/CNAME
+
 # Go To Public folder
 cd dist/public
 # Add changes to git.
